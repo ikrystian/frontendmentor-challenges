1b578eccd3d9eea0fa499e56ce40da2f5d48d51b
Krystian <krystian@bpcoders.pl>
2020-04-19 14:56:59 +0200

    Second challenge

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 import $ from 'jquery';
 
-$(document).ready(() => {
-  console.log('lorem ipsum');
-});
+if ($('body')
+  .hasClass('coming-soon-challenge')) {
+  const submit = document.querySelector('.form__submit');
+  const emailInput = document.querySelector('#email');
+  const alertIcon = document.querySelector('.form__icon');
+
+  const showErrors = () => {
+    emailInput.classList.add('form__input--error');
+    alertIcon.style.display = 'block';
+    return false;
+  };
+
+  submit.addEventListener('click', () => {
+    const isValid = $('#form')[0].checkValidity();
+    return (!isValid) ? showErrors() : true;
+  });
+};
